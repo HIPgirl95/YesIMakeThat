c8090ff9cbf4bdb3841809c3e052c75f23a1335a
HIPgirl95 <hannah@dawne.com>
2025-07-16 21:42:49 -0400

    Adding carousels

---
+++ yes-i-make-that/components/coloringBooksCarousel.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+import Image from "next/image";
+
+const image = "https://placehold.co/400x400/png";
+
+export default function MyCarousel() {
+  return (
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={20}
+      slidesPerView={1}
+      navigation
+      loop
+    >
+      <SwiperSlide>
+        <Image src={image} alt="Slide 1" width={400} height={400} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={image} alt="Slide 2" width={400} height={400} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={image} alt="Slide 3" width={400} height={400} />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
--- yes-i-make-that/pages/shop.js
+++ yes-i-make-that/pages/shop.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import ColorBooksCarousel from "../components/coloringBooksCarousel";
 
 export default function Shop() {
   return (
     <div>
       <h1>Our Shop</h1>
       <p>Explore our range of products.</p>
-      <Link href="/shopPages/nails">Nail Services</Link>
+      <Link href="/shopPages/coloringBooks">Coloring Books</Link>
+      <ColorBooksCarousel />
+      <ColorBooksCarousel />
+      <ColorBooksCarousel />
     </div>
   );
 }
