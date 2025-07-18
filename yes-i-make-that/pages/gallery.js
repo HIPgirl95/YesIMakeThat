457139a4e4383665b316f27148f04dad7afeb0c8
HIPgirl95 <hannah@dawne.com>
2025-07-17 22:44:49 -0400

    Create basic gallery carousel

--- yes-i-make-that/pages/gallery.js
+++ yes-i-make-that/pages/gallery.js
@@ -1,8 +1,11 @@
+import GalleryCarousel from "../components/galleryCarousel";
+
 export default function Gallery() {
   return (
     <div>
       <h1>Our Gallery</h1>
       <p>Check out our collection of work.</p>
+      <GalleryCarousel />
     </div>
   );
 }
