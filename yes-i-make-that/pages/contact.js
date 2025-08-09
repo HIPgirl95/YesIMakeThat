2449ee482890d7d05781a809fd5db4c56cfddd6d
HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
2025-08-09 11:39:30 -0400

    Adding contact text

--- yes-i-make-that/pages/contact.js
+++ yes-i-make-that/pages/contact.js
@@ -7,7 +7,27 @@ export default function Contact() {
   return (
     <div className={styles.contactPage}>
       <div>
-        <h1>Contact Us</h1>
+        <h1>Contact Us!</h1>
+        <h3>
+          If you have any questions, comments, or concerns, please feel free to reach out to me via email or direct message on Instagram.
+        </h3>
+        <h3>
+          I look forward to receiving your email and will respond as soon as I am able. Please feel free to ask me any questions you have about potential commission opportunities/ projects you would like to have done.
+        </h3>
+        <h3>
+          I am always happy to help and will do my best to get back to you as soon as possible.
+        </h3>
+        <h2>
+          Thank you so much, <br />
+          Courtney S.
+        </h2>
+        <h3>
+          Email me at: 
+          <br />
+          yes.imakethat@gmail.com <br />
+          OR <br />
+          Direct Message me through my instagram 
+        </h3>
       </div>
       <div>
         <p>Get in touch with us for any inquiries.</p>
